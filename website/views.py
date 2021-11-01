be6333dc5e848ef726f197d098802a50cc43bbbb
Rodney Gauna <rodneygauna@gmail.com>
2021-10-31 22:34:32 -0700

    base extension with home

--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@
 # ------------------------------------------------------------------------------
 # Imports
 # ------------------------------------------------------------------------------
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 # ------------------------------------------------------------------------------
@@ -17,4 +17,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
