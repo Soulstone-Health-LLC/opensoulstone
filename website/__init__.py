1146bc948aefe2caa05224f9603c475faf3b0ed5
Rodney Gauna <rodneygauna@gmail.com>
2021-10-31 22:03:34 -0700

    blueprint views

--- website/__init__.py
+++ website/__init__.py
@@ -32,4 +32,10 @@ def create_app():
     # Flask secret key configuration
     app.config['SECRET_KEY'] = secret_key
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
