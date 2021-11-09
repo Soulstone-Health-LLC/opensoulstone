43dce820a68c3446a3d0e831fb3e1c2e0e62552c
Rodney Gauna <rodneygauna@gmail.com>
2021-11-09 12:03:52 -0800

    saves practice

--- website/views.py
+++ website/views.py
@@ -9,7 +9,7 @@
 # ------------------------------------------------------------------------------
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-# from .models import Pet
+from .models import Practice
 from . import db
 
 
@@ -34,7 +34,26 @@ def support():
     return render_template("support.html", user=current_user)
 
 
-@views.route('/support/add_practice')
+@views.route('/support/add_practice', methods=['GET', 'POST'])
 @login_required
 def addpractice():
+    '''Add practice form and page'''
+    # Gets the data from the form and saves as variables
+    if request.method == 'POST':
+        practicename = request.form.get('practicename')
+        biography = request.form.get('biography')
+        email = request.form.get('email')
+        website = request.form.get('website')
+        phonenumber = request.form.get('phonenumber')
+        phonetype = request.form.get('phonetype')
+        
+        # Add new user to database
+        new_practice = Practice(name=practicename, biography=biography,
+                                email=email, website=website,
+                                phone_number=phonenumber,
+                                phone_type=phonetype)
+        db.session.add(new_practice)
+        db.session.commit()
+        flash(' Account created!', category='success')
+        return redirect(url_for('views.support'))
     return render_template("add_practice.html", user=current_user)
