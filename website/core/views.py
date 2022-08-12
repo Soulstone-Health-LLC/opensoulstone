ec802f5ace1e953ae3cdb7b962b5074d8e200025
Rodney Gauna <rodneygauna@gmail.com>
2022-08-11 22:44:58 -0700

    refactor - core

--- website/core/views.py
+++ website/core/views.py
@@ -1,1 +1,23 @@
 # soulstone/website/core/views.py
+
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+
+# ------------------------------------------------------------------------------
+# Global Variables
+# ------------------------------------------------------------------------------
+core = Blueprint('core', __name__)
+
+
+# ------------------------------------------------------------------------------
+# Routes - Practice - Dashboard
+# ------------------------------------------------------------------------------
+# Homepage
+@core.route('/')
+@login_required
+def home():
+    return render_template("home.html", title="Soulstone", user=current_user)
