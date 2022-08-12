4116fe053b9e471c771d50591bed2bc8c390e9a8
Rodney Gauna <rodneygauna@gmail.com>
2022-08-11 22:55:24 -0700

    403 error page

--- website/error_pages/handler.py
+++ website/error_pages/handler.py
@@ -17,13 +17,20 @@ error_pages = Blueprint('error_pages', __name__)
 # ------------------------------------------------------------------------------
 # 401 Page
 @error_pages.errorhandler(401)
-def page_401(e):
+def page_401(error):
     ''' Displays 401 page if there is an error '''
     return render_template("401.html", title='Soulstone'), 401
 
 
+# 403 Page
+@error_pages.errorhandler(403)
+def page_403(error):
+    ''' Displays 403 page if there is an error '''
+    return render_template("403.html", title='Soulstone'), 403
+
+
 # 404 Page
 @error_pages.errorhandler(404)
-def page_404(e):
+def page_404(error):
     ''' Displays 404 page if there is an error '''
     return render_template("404.html", title='Soulstone')
