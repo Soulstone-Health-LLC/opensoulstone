be76f82827fb74ac3b508a99b867eae2c48bfb77
Rodney Gauna <rodneygauna@gmail.com>
2023-05-10 17:36:19 +0000

    lint and format fixes

--- src/support/forms.py
+++ src/support/forms.py
@@ -1,70 +1,66 @@
-# soulstone/website/support/forms.py
+"""
+Support > Forms - This file contains all of the forms for the Support
+Blueprint.
+"""
 
-
-# ------------------------------------------------------------------------------
 # Imports
-# ------------------------------------------------------------------------------
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, EmailField
 from wtforms.fields.simple import TelField
 from wtforms.validators import DataRequired, Length, Email
 from wtforms.widgets import TextArea
-from src.dictionary.general import (STATE_CHOICES, ROLE_CHOICES,
-                                    PHONE_TYPE_CHOICES)
+from src.dictionary.general import (
+    STATE_CHOICES,
+    ROLE_CHOICES,
+    PHONE_TYPE_CHOICES,
+)
 
 
-# ------------------------------------------------------------------------------
 # Form - Support - Add New Practice
-# ------------------------------------------------------------------------------
 class AddPracticeForm(FlaskForm):
-    ''' Add New Practice Form '''
-    name = StringField(label='Practice Name *',
-                             validators=[DataRequired(),
-                                         Length(min=2)])
-    biography = StringField(label='Practice Biography',
-                            widget=TextArea())
-    address_1 = StringField(label='Address Line 1 *',
+    """Add New Practice Form"""
+
+    name = StringField(
+        label="Practice Name *", validators=[DataRequired(), Length(min=2)]
+    )
+    biography = StringField(label="Practice Biography", widget=TextArea())
+    address_1 = StringField(label="Address Line 1 *",
                             validators=[DataRequired()])
-    address_2 = StringField(label='Address Line 2')
-    city = StringField(label='City *',
-                       validators=[DataRequired()])
-    state = SelectField(label='State *',
-                        choices=STATE_CHOICES,
-                        validators=[DataRequired()])
-    zipcode = StringField(label='Zipcode *',
-                          validators=[DataRequired()])
-    email = EmailField(label='Practice Email *',
-                       validators=[Email()])
-    website = StringField(label='Practice Website')
-    phone = TelField(label='Practice Phone Number *',
+    address_2 = StringField(label="Address Line 2")
+    city = StringField(label="City *", validators=[DataRequired()])
+    state = SelectField(
+        label="State *", choices=STATE_CHOICES, validators=[DataRequired()]
+    )
+    zipcode = StringField(label="Zipcode *", validators=[DataRequired()])
+    email = EmailField(label="Practice Email *", validators=[Email()])
+    website = StringField(label="Practice Website")
+    phone = TelField(label="Practice Phone Number *",
                      validators=[DataRequired()])
-    phone_type = SelectField(label='Phone Type *',
-                             choices=PHONE_TYPE_CHOICES)
-    submit = SubmitField(label='Add New Practice')
+    phone_type = SelectField(label="Phone Type *", choices=PHONE_TYPE_CHOICES)
+    submit = SubmitField(label="Add New Practice")
 
 
-# ------------------------------------------------------------------------------
 # Form - Add Practice User
-# ------------------------------------------------------------------------------
 class PracticeUserForm(FlaskForm):
-    ''' Add or Edit the User to the Practice Form '''
-    role = SelectField(label='User Role *',
-                       choices=ROLE_CHOICES,
-                       validators=[DataRequired()])
-    first_name = StringField(label='First Name *',
-                             validators=[DataRequired(),
-                                         Length(min=2, max=150)])
-    middle_name = StringField(label='Middle Name')
-    last_name = StringField(label='Last Name *',
-                            validators=[DataRequired(),
-                                        Length(min=2, max=150)])
-    suffix_name = StringField(label='Suffix')
-    email = EmailField(label='Email *',
-                       validators=[DataRequired(),
-                                   Email(),
-                                   Length(min=3, max=150)])
-    phone = TelField(label='Phone Number *',
-                     validators=[DataRequired()])
-    phone_type = SelectField(label='Phone Type *',
-                             choices=PHONE_TYPE_CHOICES)
-    submit = SubmitField(label='Register Account')
+    """Add or Edit the User to the Practice Form"""
+
+    role = SelectField(
+        label="User Role *", choices=ROLE_CHOICES, validators=[DataRequired()]
+    )
+    first_name = StringField(
+        label="First Name *", validators=[DataRequired(),
+                                          Length(min=2, max=150)]
+    )
+    middle_name = StringField(label="Middle Name")
+    last_name = StringField(
+        label="Last Name *", validators=[DataRequired(),
+                                         Length(min=2, max=150)]
+    )
+    suffix_name = StringField(label="Suffix")
+    email = EmailField(
+        label="Email *", validators=[DataRequired(), Email(),
+                                     Length(min=3, max=150)]
+    )
+    phone = TelField(label="Phone Number *", validators=[DataRequired()])
+    phone_type = SelectField(label="Phone Type *", choices=PHONE_TYPE_CHOICES)
+    submit = SubmitField(label="Register Account")
--- src/support/views.py
+++ src/support/views.py
@@ -1,8 +1,8 @@
-# soulstone/website/support/views.py
+"""
+Support > Views - This file contains the views for the Support Blueprint.
+"""
 
-# ------------------------------------------------------------------------------
 # Imports
-# ------------------------------------------------------------------------------
 import random
 import string
 from flask import Blueprint, render_template, request, flash, redirect, url_for
@@ -13,53 +13,49 @@ from src import db
 from src.models import Practice, User
 
 
-# ------------------------------------------------------------------------------
-# Global Variables
-# ------------------------------------------------------------------------------
-supportapp = Blueprint('supportapp', __name__)
+# Blueprint Configuration
+supportapp = Blueprint("supportapp", __name__)
 
 
-# ------------------------------------------------------------------------------
-# Routes - Support
-# ------------------------------------------------------------------------------
 # Support - Practices
-@supportapp.route('/support')
+@supportapp.route("/support")
 @login_required
 def support():
     if request.method == "GET":
         if current_user.role == "Support":
             practices = Practice.query.order_by(Practice.id).all()
 
-            return render_template("support/support.html",
-                                   user=current_user,
-                                   practices=practices)
+            return render_template(
+                "support/support.html", user=current_user, practices=practices
+            )
         else:
-            return render_template("401.html",
-                                   user=current_user)
+            return render_template("401.html", user=current_user)
 
 
 # Support - Practices - View Practice
-@supportapp.route('/support/<int:id>')
+@supportapp.route("/support/<int:id>")
 @login_required
 def viewpractice(id):
     practice = Practice.query.get_or_404(id)
     practice_user = User.query.order_by(User.last_name).all()
-    return render_template("support/support_practice.html",
-                           user=current_user,
-                           practice=practice,
-                           practice_user=practice_user)
+    return render_template(
+        "support/support_practice.html",
+        user=current_user,
+        practice=practice,
+        practice_user=practice_user,
+    )
 
 
 # Support - Practices - Add Practice
-@supportapp.route('/support/add_practice', methods=['GET', 'POST'])
+@supportapp.route("/support/add_practice", methods=["GET", "POST"])
 @login_required
 def addpractice():
-    '''Add practice form and page'''
+    """Add practice form and page"""
     form = AddPracticeForm()
 
     # Gets the data from the form and saves as variables
     if form.validate_on_submit():
-        if request.method == 'POST':
+        if request.method == "POST":
             name = form.name.data
             biography = form.biography.data
             address_1 = form.address_1.data
@@ -73,50 +69,54 @@ def addpractice():
             phone_type = form.phone_type.data
 
         # Add new practice to database
-        new_practice = Practice(name=name,
-                                biography=biography,
-                                address_1=address_1,
-                                address_2=address_2,
-                                city=city,
-                                state=state,
-                                zipcode=zipcode,
-                                email=email,
-                                website=website,
-                                phone_number=phone_number,
-                                phone_type=phone_type)
+        new_practice = Practice(
+            name=name,
+            biography=biography,
+            address_1=address_1,
+            address_2=address_2,
+            city=city,
+            state=state,
+            zipcode=zipcode,
+            email=email,
+            website=website,
+            phone_number=phone_number,
+            phone_type=phone_type,
+        )
         db.session.add(new_practice)
         db.session.commit()
-        flash(f'{name} created successfully.', category='success')
+        flash(f"{name} created successfully.", category="success")
 
         # Redirect user to the Support home page
-        return redirect(url_for('views.support'))
+        return redirect(url_for("views.support"))
 
-    return render_template("support/add_practice.html",
-                           title="Soulstone - Add Practice",
-                           form=form,
-                           user=current_user)
+    return render_template(
+        "support/add_practice.html",
+        title="Soulstone - Add Practice",
+        form=form,
+        user=current_user,
+    )
 
 
 # Support - Practices - Add Practice User
-@supportapp.route('/support/<int:id>/support_add_user',
-                  methods=['GET', 'POST'])
+@supportapp.route("/support/<int:id>/support_add_user",
+                  methods=["GET", "POST"])
 @login_required
 def addPracticeUser(id):
-    '''Add practice user form and page'''
+    """Add practice user form and page"""
     form = PracticeUserForm()
     practice = Practice.query.get(id)
 
     # Random string
     def randompass(length):
-        ''' Generates a random string for a temporary password '''
+        """Generates a random string for a temporary password"""
         s = string
         letters = s.ascii_lowercase + s.ascii_uppercase + s.digits
-        return ''.join(random.choice(letters) for i in range(length))
+        return "".join(random.choice(letters) for i in range(length))
 
     # Gets the data from the form and saves as variables
     if form.validate_on_submit():
-        if request.method == 'POST':
-            practice_id = request.form.get('practice_id')
+        if request.method == "POST":
+            practice_id = request.form.get("practice_id")
             role = form.role.data
             firstname = form.first_name.data
             middlename = form.middle_name.data
@@ -128,27 +128,30 @@ def addPracticeUser(id):
             password = randompass(10)
 
             # Add new practice user to database
-            new_practice_user = User(practice_id=practice_id,
-                                     role=role,
-                                     first_name=firstname,
-                                     middle_name=middlename,
-                                     last_name=lastname,
-                                     suffix_name=suffix,
-                                     email=email,
-                                     password=generate_password_hash(
-                                         password, method='sha384'),
-                                     phone_number=phonenumber,
-                                     phone_type=phonetype,
-                                     status='Active')
+            new_practice_user = User(
+                practice_id=practice_id,
+                role=role,
+                first_name=firstname,
+                middle_name=middlename,
+                last_name=lastname,
+                suffix_name=suffix,
+                email=email,
+                password=generate_password_hash(password, method="sha384"),
+                phone_number=phonenumber,
+                phone_type=phonetype,
+                status="Active",
+            )
             db.session.add(new_practice_user)
             db.session.commit()
-            flash(f'{form.email} created successfully.', category='success')
+            flash(f"{form.email} created successfully.", category="success")
 
             # Redirect user to Support home page
-            return redirect(url_for('supportapp.support'))
+            return redirect(url_for("supportapp.support"))
 
-    return render_template("support/support_add_user.html",
-                           title="Soulstone - Add Practice User",
-                           user=current_user,
-                           form=form,
-                           practice=practice)
+    return render_template(
+        "support/support_add_user.html",
+        title="Soulstone - Add Practice User",
+        user=current_user,
+        form=form,
+        practice=practice,
+    )
