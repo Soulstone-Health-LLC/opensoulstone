15b4ccf4f120bf7a697ebd2c9f718990e13c74e7
Rodney Gauna <rodneygauna@gmail.com>
2023-05-18 21:53:23 +0000

    Start of the TOS add and edit form

--- src/terms_of_service/forms.py
+++ src/terms_of_service/forms.py
@@ -3,12 +3,29 @@ Terms of Service - User Agreement Forms
 """
 
 # Imports
+from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, TextAreaField, StringField, DateField
+from wtforms.validators import DataRequired
 
 
 # User Agreement Form
 class UserAgreementForm(FlaskForm):
     """User Agreement Form"""
     submit = SubmitField("I Agree")
+    cancel = SubmitField('Cancel', render_kw={'formnovalidate': True})
+
+
+class TermsOfServiceForm(FlaskForm):
+    """Terms of Service Form"""
+    current_date = datetime.now()
+
+    content = TextAreaField(
+        "Content * (HTML)", render_kw={"rows": 10}, validators=[DataRequired()])
+    active_date = DateField(
+        "Active Date *", format="%Y-%m-%d", validators=[DataRequired()])
+    sunset_date = DateField("Sunset Date", format="%Y-%m-%d")
+    version = StringField(
+        "Version *", default=str(current_date), validators=[DataRequired()])
+    submit = SubmitField("Submit")
     cancel = SubmitField('Cancel', render_kw={'formnovalidate': True})
