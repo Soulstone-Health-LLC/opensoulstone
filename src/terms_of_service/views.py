665951a9c735193af59c39862ec0c578a1aa8151
Rodney Gauna <rodneygauna@gmail.com>
2023-05-18 21:53:06 +0000

    user can view a TOS details

--- src/terms_of_service/views.py
+++ src/terms_of_service/views.py
@@ -7,8 +7,9 @@ from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import current_user, login_required
 from datetime import datetime
 from src.terms_of_service.forms import UserAgreementForm
-from src.models import UserAgreement, TermsOfService
+from src.models import UserAgreement, TermsOfService, User
 from src import db
+from sqlalchemy.orm import aliased
 from src.decorators.decorators import support_required
 
 
@@ -67,15 +68,39 @@ def support_list_tos():
 
 
 # Support - Terms of Service - View Terms of Service
-@terms_of_service.route("/support/terms_of_service/<int:tos_id>",
-                        methods=["GET"])
+@terms_of_service.route("/support/terms_of_service/<int:tos_id>", methods=["GET"])
 @login_required
 @support_required
 def support_view_tos(tos_id):
     """Support - Terms of Service - View Terms of Service"""
 
+    # Create aliases for the User table
+    created_by_user = aliased(User)
+    updated_by_user = aliased(User)
+
     # Get terms of service
-    tos = TermsOfService.query.filter_by(id=tos_id).first()
+    tos = (
+        db.session.query(
+            TermsOfService.id,
+            TermsOfService.version,
+            TermsOfService.content,
+            TermsOfService.created_at,
+            TermsOfService.created_by,
+            TermsOfService.updated_at,
+            TermsOfService.updated_by,
+            TermsOfService.active_date,
+            TermsOfService.sunset_date,
+            created_by_user.first_name.label('created_by_first_name'),
+            created_by_user.last_name.label('created_by_last_name'),
+            updated_by_user.first_name.label('updated_by_first_name'),
+            updated_by_user.last_name.label('updated_by_last_name'),
+        )
+        .join(created_by_user, TermsOfService.created_by == created_by_user.id)
+        .outerjoin(updated_by_user,
+                   TermsOfService.updated_by == updated_by_user.id)
+        .filter(TermsOfService.id == tos_id)
+        .first()
+    )
 
     return render_template("terms_of_service/support_view_tos.html",
                            user=current_user,
