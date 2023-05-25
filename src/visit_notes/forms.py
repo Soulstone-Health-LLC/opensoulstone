04b481057e1c76d7148127afacb523f013f969c8
Rodney Gauna <rodneygauna@gmail.com>
2023-05-25 16:19:45 +0000

    text area rows improvements

--- src/visit_notes/forms.py
+++ src/visit_notes/forms.py
@@ -20,52 +20,62 @@ class AddVisitNoteForm(FlaskForm):
         label="Date of Service *", validators=[DataRequired()],
         default=datetime.today
     )
-    reason_for_visit = StringField(label="Reason for Visit", widget=TextArea())
+    reason_for_visit = StringField(label="Reason for Visit", widget=TextArea(),
+                                   render_kw={"rows": 5})
     chakra_assessment_root_score = SelectField(
         label="Root Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_root_description = StringField(
-        label="Root Chakra Notes", widget=TextArea()
+        label="Root Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_sacral_score = SelectField(
         label="Sacral Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_sacral_description = StringField(
-        label="Sacral Chakra Notes", widget=TextArea()
+        label="Sacral Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_solar_plexus_score = SelectField(
         label="Solar Plexus Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_solar_plexus_description = StringField(
-        label="Solar Plexus Chakra Notes", widget=TextArea()
+        label="Solar Plexus Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_heart_score = SelectField(
         label="Heart Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_heart_description = StringField(
-        label="Heart Chakra Notes", widget=TextArea()
+        label="Heart Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_throat_score = SelectField(
         label="Throat Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_throat_description = StringField(
-        label="Throat Chakra Notes", widget=TextArea()
+        label="Throat Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_third_eye_score = SelectField(
         label="Third-Eye Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_third_eye_description = StringField(
-        label="Third-Eye Chakra Notes", widget=TextArea()
+        label="Third-Eye Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_crown_score = SelectField(
         label="Crown Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_crown_description = StringField(
-        label="Crown Chakra Notes", widget=TextArea()
+        label="Crown Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
-    visit_notes = StringField(label="Visit Note", widget=TextArea())
+    visit_notes = StringField(label="Visit Note", widget=TextArea(),
+                              render_kw={"rows": 10})
     post_visit_recommendations = StringField(
-        label="Post Visit Recommendations", widget=TextArea()
+        label="Post Visit Recommendations", widget=TextArea(),
+        render_kw={"rows": 10}
     )
     submit = SubmitField(label="Create Visit Note")
 
@@ -76,51 +86,61 @@ class EditVisitNoteForm(FlaskForm):
 
     date_of_service = DateField(
         label="Date of Service *", validators=[DataRequired()])
-    reason_for_visit = StringField(label="Reason for Visit", widget=TextArea())
+    reason_for_visit = StringField(label="Reason for Visit", widget=TextArea(),
+                                   render_kw={"rows": 5})
     chakra_assessment_root_score = SelectField(
         label="Root Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_root_description = StringField(
-        label="Root Chakra Notes", widget=TextArea()
+        label="Root Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_sacral_score = SelectField(
         label="Sacral Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_sacral_description = StringField(
-        label="Sacral Chakra Notes", widget=TextArea()
+        label="Sacral Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_solar_plexus_score = SelectField(
         label="Solar Plexus Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_solar_plexus_description = StringField(
-        label="Solar Plexus Chakra Notes", widget=TextArea()
+        label="Solar Plexus Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_heart_score = SelectField(
         label="Heart Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_heart_description = StringField(
-        label="Heart Chakra Notes", widget=TextArea()
+        label="Heart Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_throat_score = SelectField(
         label="Throat Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_throat_description = StringField(
-        label="Throat Chakra Notes", widget=TextArea()
+        label="Throat Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_third_eye_score = SelectField(
         label="Third-Eye Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_third_eye_description = StringField(
-        label="Third-Eye Chakra Notes", widget=TextArea()
+        label="Third-Eye Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
     chakra_assessment_crown_score = SelectField(
         label="Crown Chakra Score", choices=CHAKRA_SCORE_CHOICES
     )
     chakra_assessment_crown_description = StringField(
-        label="Crown Chakra Notes", widget=TextArea()
+        label="Crown Chakra Notes", widget=TextArea(),
+        render_kw={"rows": 5}
     )
-    visit_notes = StringField(label="Visit Note", widget=TextArea())
+    visit_notes = StringField(label="Visit Note", widget=TextArea(),
+                              render_kw={"rows": 10})
     post_visit_recommendations = StringField(
-        label="Post Visit Recommendations", widget=TextArea()
+        label="Post Visit Recommendations", widget=TextArea(),
+        render_kw={"rows": 10}
     )
     submit = SubmitField(label="Save Visit Note")
