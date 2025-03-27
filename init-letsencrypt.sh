87a27b52a0d61e1392692e7b9ca2d56e5d2178d8
Rodney Gauna <rodneygauna@gmail.com>
2025-03-26 23:01:30 -0700

    fix: change domains variable from array to string in init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(soulstone.app)
+domains="soulstone.app"
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="rodney@soulstonehealth.com" # Adding a valid address is strongly recommended
